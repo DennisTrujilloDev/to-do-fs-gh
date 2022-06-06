ba0c0f6012fe2c023c225c83790062365588f4fb
DennisTrujilloDev <dennis.joel.trujillo@gmail.com>
2022-06-05 21:13:25 -0400

    update conditional

--- index.js
+++ index.js
@@ -1,46 +1,81 @@
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 
-app.set('view engine', 'ejs')
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(bodyParser.json())
-app.use(express.static('views'))
+var db, collection;
 
-let todos = {} // temp DB
+const url =
+  "mongodb+srv://DennisTrujilloDev:hiHello@cluster0.olwy9tk.mongodb.net/?retryWrites=true&w=majority";
+const dbName = "to-do";
 
-app.get('/', (req, res)=>{
-  res.render('index.ejs', {todos})
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(express.static("views"));
 
-})
+app.get("/", (req, res) => {
+  db.collection("to-do-items")
+    .find()
+    .toArray((err, toDoArray) => {
+      console.log(toDoArray);
+      if (err) return console.log(err);
+      res.render("index.ejs", { todos: toDoArray });
+    });
+});
 
-app.post('/addToDo', (req, res)=> { //req from browser, res is what will be sent back via ejs aka html
-  const newToDo = {name: req.body.toDo, isCompleted: false, id: new Date().toString()}
+app.post("/addToDo", (req, res) => {
+  //req from browser, res is what will be sent back via ejs aka html
+  const newToDo = {
+    name: req.body.toDo,
+    isCompleted: false,
+    id: new Date().toString(),
+  };
+  db.collection("to-do-items").insertOne(newToDo, (err, result) => {
+    if (err) return console.log(err);
+    console.log("saved to database");
+    res.redirect("/");
+  });
+});
 
-  todos[newToDo.id] = newToDo
-  console.log(newToDo);
-  res.redirect('/') // anytime you land on homepage you render index.ejs file w data already
-}) 
+app.put("/updateIt", (req, res) => {
+  db.collection("to-do-items").findOneAndUpdate(
+    { id: req.body.id },
+    {
+      $set: {
+        isCompleted: true,
+      },
+    },
+    {
+      sort: { _id: -1 },
+      upsert: true,
+    },
+    (err, result) => {
+      if (err) return res.send(err);
+      res.send(result);
+    }
+  );
+});
 
-app.delete('/messages', (req, res) => {
-  let plsDelete = req.body.id.split(";")
-  plsDelete.forEach(elementToDelete => {
-    delete todos[elementToDelete]
-  })
-  // delete todos[req.body.id]
-  // console.log(todos);
-    res.send('Message deleted!')
-})
+app.delete("/deleteAll", (req, res) => {
+  db.collection("to-do-items").findOneAndDelete({id: req.body.id}, (err, result) => {
+    if (err) return res.send(500, err);
+    res.send("Message deleted!");
+  });
+});
 
+app.listen(7007, () => {
+  MongoClient.connect(
+    url,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    (error, client) => {
+      if (error) {
+        throw error;
+      }
+      db = client.db(dbName);
+      console.log("Connected to `" + dbName + "`!");
+    }
+  );
 
-
-
-
-
-
-
-
-
-
-
-app.listen(7007, () => console.log('running port 7007'))
+  console.log("running port 7007");
+});
--- views/client.js
+++ views/client.js
@@ -1,39 +1,42 @@
-function handleSubmit(event){
-  event.preventDefault() 
-  //stops page from refreshing b/c thats default behav for form 
+function handleSubmit(event) {
+  event.preventDefault();
+  //stops page from refreshing b/c thats default behav for form
 }
+const listItem = document.querySelectorAll("li");
+const clearList = document.querySelector("#byeByeList");
 
-  const input = document.querySelector("#input")
-  const ul = document.querySelector ("#ul")
-  const clearList = document.querySelector("#byeByeList")
-  
-  ul.addEventListener("click", lineThruToDoTask)
+Array.from(listItem).forEach((ele) => {
+  ele.addEventListener("click", updateLI);
+});
+function updateLI(e) {
+  let itemID = e.target.getAttribute("data-id");
+  fetch("updateIt", {
+    method: "put",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: itemID,
+    }),
+  }).then(function (response) {
+    console.log(response);
+    window.location.reload();
+  });
+}
+clearList.addEventListener("click", eraseTask);
+function eraseTask() {
+  const listItem = document.querySelectorAll(".line");
+  const IDsToDelete = [];
+  Array.from(listItem).forEach((ele) => {
+    IDsToDelete.push(ele.getAttribute("data-id"));
+  });
 
-  document.querySelector("#byeByeTask").addEventListener("click", eraseTask) 
-  
-  function lineThruToDoTask(e){
-    if(e.target.classList.contains("line")){
-      e.target.classList.toggle("strikeThrough")
-    }
-  }
-
-  function eraseTask(){
-    const listItem = document.querySelectorAll('.strikeThrough')
-    let IDsToDelete = []
-    Array.from(listItem).forEach((ele)=>{
-      IDsToDelete.push(ele.getAttribute('data-id'))
-    })
-
-    fetch('messages', {
-      method: 'delete',
-      headers: {'Content-Type': 'application/json'},
-      body: JSON.stringify({
-        'id': IDsToDelete.join(';')
-      })
-    }).then(function (response) {
-      console.log(response);
-      window.location.reload()
-    })
-};
-
-
+  fetch("deleteAll", {
+    method: "delete",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      id: IDsToDelete.join(";"),
+    }),
+  }).then(function (response) {
+    console.log(response);
+    window.location.reload();
+  });
+}
